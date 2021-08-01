ca0ac3264aaf6a3974f71088a50b504be459b0d2
bondarM <mihailbondar94@gmail.com>
2021-08-01 16:19:44 +0300

    render results

--- src/components/RoundList.js
+++ src/components/RoundList.js
@@ -3,9 +3,7 @@ import { RoundResult } from './RoundResult'
 
 export const RoundList = () => {
     const [rounds, setRounds] = useState([])
-    const [itemsBlock, setItemsBlock] = useState([])
-    const [itemsHeight, setItemsHeight] = useState("")
-    const [activeList, setActiveList] = useState(-1);
+    // const [itemsHeight, setItemsHeight] = useState("")
 
     useEffect(() => {
         getRounds()
@@ -23,64 +21,6 @@ export const RoundList = () => {
     }
 
 
-    const infoRound = (id) => {
-        fetch(`https://60f7b35b9cdca00017454f5e.mockapi.io/api/v1/round/${id}`)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                let list = data.items.split(",")
-                let matrix = listToMatrix(list, list.length / data.height)
-                setItemsBlock(matrix);
-            })
-    }
-
-
-    function listToMatrix(list, elementsPerSubArray) {
-        let matrix = [], i, k;
-        for (i = 0, k = -1; i < list.length; i++) {
-            if (i % elementsPerSubArray === 0) {
-                k++;
-                matrix[k] = [];
-            }
-            matrix[k].push(list[i]);
-        }
-        return matrix;
-    }
-
-    const addActive = (e, tab) => {
-        activeList === tab ? setActiveList(0) : setActiveList(tab);
-    }
-
-
-
-
-    //     return (
-    //         <div className="roundlist">
-    //             <table>
-    //                 <tr>
-    //                     <th>Rounds</th>
-    //                     <th>Date</th>
-    //                 </tr>
-    //                 {rounds.map(round => (
-    //                     <>
-    //                         <tr onClick={(e) => { infoRound(round.roundId); addActive(e, round.roundId) }} key={round.dateTime}>
-    //                             <th>{round.roundId}</th>
-    //                             <th>{round.dateTime}</th>
-    //                         </tr>
-    //                         {(activeList === round.roundId) ?
-    //                           <RoundResult itemsBlock={itemsBlock} />
-    //                             :
-    //                             ""
-    //                         }
-    //                     </>
-    //                 ))}
-    //             </table>
-    //         </div>
-    //     )
-    // }
-
-
     return (
         <div className="roundlist">
             <table>
@@ -88,33 +28,9 @@ export const RoundList = () => {
                     <th>Rounds</th>
                     <th>Date</th>
                 </tr>
-
                 {rounds.map(round => (
-                    <>
-                        <RoundResult roundId={round.roundId} dateTime={round.dateTime} infoRound={infoRound} rounds={rounds} >
-
-                            <div className="roundlist__items">{itemsBlock.length ? itemsBlock.map((item, i) => {
-                                return <div className="roundlist__row">
-
-                                    {item.map((img, index) => {
-                                        if (img == 4) {
-                                            return <img key={index} className={`items__image item__4 item__position__${i}`} src={require(`../images/${img}.png`).default} />
-                                        } else {
-                                            return <img key={index} className="items__image" src={require(`../images/${img}.png`).default} />
-                                        }
-                                    })}
-                                    
-                                </div>
-                            }) : <div></div>}</div>
-
-                        </RoundResult>
-
-                    </>
+                        <RoundResult roundId={round.roundId} dateTime={round.dateTime} rounds={rounds} />
                 ))}
-
-
-
-
             </table>
         </div>
     )
--- src/components/RoundResult.js
+++ src/components/RoundResult.js
@@ -1,29 +1,58 @@
 import React, { useState } from 'react'
 
-export const RoundResult = ({ rounds, infoRound, children, roundId, dateTime }) => {
-    // return (
-    //     <div className="roundlist__items">{itemsBlock.length ? itemsBlock.map((item, i) => {
-    //         return <div className="roundlist__row">
-    //             {item.map((img, index) => {
-    //                 if (img == 4) {
-    //                     return <img key={index} className={`items__image item__4 item__position__${i}`} src={require(`../images/${img}.png`).default} />
-    //                 } else {
-    //                     return <img key={index} className="items__image" src={require(`../images/${img}.png`).default} />
-    //                 }
-    //             })}
-    //         </div>
-    //     }) : ""}</div>
-    // )
-    const [open, setOpen] = useState(false)
+export const RoundResult = ({ rounds, children, roundId, dateTime }) => {
 
+    const [open, setOpen] = useState(false)
+    const [itemsBlock, setItemsBlock] = useState([])
+
+    const infoRound = (id) => {
+        fetch(`https://60f7b35b9cdca00017454f5e.mockapi.io/api/v1/round/${id}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                let list = data.items.split(",")
+                let matrix = listToMatrix(list, list.length / data.height)
+                setItemsBlock(matrix);
+            })
+    }
+
+
+    function listToMatrix(list, elementsPerSubArray) {
+        let matrix = [], i, k;
+        for (i = 0, k = -1; i < list.length; i++) {
+            if (i % elementsPerSubArray === 0) {
+                k++;
+                matrix[k] = [];
+            }
+            matrix[k].push(list[i]);
+        }
+        return matrix;
+    }
     return (
         <>
             <tr onClick={(e) => { infoRound(roundId); setOpen(!open) }} key={dateTime}>
                 <th>{roundId}</th>
                 <th>{dateTime}</th>
             </tr>
-            
-            <div className={`${open ? "active" : ""}`}> {children} </div>
+
+            <div className={`${open ? "active" : ""}`}>
+                <div className="roundlist__items">
+                    {itemsBlock.length ? itemsBlock.map((item, i) => {
+                        return <div className="roundlist__row">
+
+                            {item.map((img, index) => {
+                                if (img == 4) {
+                                    return <img key={index} className={`items__image item__4 item__position__${i}`} src={require(`../images/${img}.png`).default} />
+                                } else {
+                                    return <img key={index} className="items__image" src={require(`../images/${img}.png`).default} />
+                                }
+                            })}
+
+                        </div>
+                    }) : <div></div>}
+                </div>
+            </div>
         </>
     )
 }
